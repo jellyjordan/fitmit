8e3b3e9e1e748a5ad6ac71c8c3eddbe2f2ea6edf
Jordan La Croix <jordanlacroix@gmail.com>
2015-01-01 16:53:35 -0500

    Setting the profile now syncs it with the time

--- src/account/Session.java
+++ src/account/Session.java
@@ -14,12 +14,26 @@ public class Session {
         return session;
     }
 
+    /*
+        Binds a profile to the session, and syncs the profile
+        with the current time which updates daily activities
+     */
     public static void setProfile(Profile profile) {
+        /*
+            Users must log out and start a new session with
+            a new profile in order to access another account
+         */
         if(profile == null){
             Session.profile = profile;
+            profile.syncProfile();
         }
     }
 
+    /*
+        Gives global access the profile which is binded to
+        the session, which makes it easier for the controllers
+        to make alterations
+     */
     public static Profile getProfile(){
         return profile;
     }
